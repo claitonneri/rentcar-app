b00df110eaf25a676c98a16ecbdb126e0fa74774
claitonneri <claitondidone@gmail.com>
2021-06-17 19:13:04 -0300

    feat: inclusão dos assets e Header da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from './styles';
+import Logo from '../../assets/logo.svg';
+
+import { Container, Header, Content, Total } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Title>HOME</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Content>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <Total>Total de 12 carros</Total>
+        </Content>
+      </Header>
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,16 +1,33 @@
 import styled, { css } from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
   ${({ theme }) => css`
-    font-family: ${theme.fonts.secondary_500};
-    font-size: 30px;
+    width: 100%;
+    height: 113px;
+
+    background-color: ${theme.colors.header};
+    justify-content: flex-end;
+
+    padding: 24px;
   `};
+`;
+
+export const Content = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Total = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${RFValue(15)}px;
+    font-family: ${theme.fonts.primary_400};
+    color: ${theme.colors.title};
+  `}
 `;
